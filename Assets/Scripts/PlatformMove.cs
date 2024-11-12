fc454d9a6aa393ae0ebb6f51e57a177c6b487ea8
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2024-11-12 12:53:06 +0000

    movement update unity edition

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+
+    public float moveDistance = 5.0f; 
+    public float moveSpeed = 2.0f;
+    private Vector3 targetPosition;
+    private bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            targetPosition = new Vector3(transform.position.x + moveDistance, transform.position.y, transform.position.z);
+            moving = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+                if (moving)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                    if (transform.position == targetPosition)
+                    {
+                        moving = false;
+                    }
+                }
+    }
+}
